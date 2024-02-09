2796bc39003ecb65d049015dc5c3ca0384a12f88
Nathan Wu <c4f0q@students.cs.ubc.ca>
2024-02-08 23:54:39 -0800

    Added test for Bet constructor

---
+++ src/test/model/BetsTest.java
@@ -0,0 +1,32 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BetsTest {
+
+    Bets b1;
+    Bets b2;
+    Bets b3;
+
+    @BeforeEach
+    void runBefore() {
+
+        b2 = new Bets("LeBron James Over 7.5 Assists", 100, 2, false);
+        b3 = new Bets("Anthony Davis Over 3.5 Blocks", 100, 2.5, true);
+    }
+    @Test
+    void Bets() {
+
+        b1 = new Bets("LeBron James Over 25.5 Points", 100, 2, true);
+        assertEquals(100, b1.getProfit());
+        assertEquals(100, b1.getAmountPlaced());
+        assertTrue(b1.getWin());
+
+    }
+}
+
+
